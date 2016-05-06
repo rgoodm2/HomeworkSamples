8acaa5995ab8d808412d01b3d8fe597b70706b1b
Rachel Goodman <rachel.goodman@emory.edu>
2016-05-06 08:58:10 -0400

    Update primes.c

--- primes.c
+++ primes.c
@@ -1,9 +1,32 @@
 
-#include<header.h>
-#include<main.c>
-#include<main.o>
+#include "header.h"
 
+void setBit( int k ) {
 
+   int i = k/32;
+   int pos = k%32;
+   unsigned int flag = 1;
+   flag = flag << pos;
+   prime[i] = prime[i] | flag;
+}
+
+/* -------------------------------------------------
+   testBit( n ): return 1 if bit n is RESET
+   ------------------------------------------------- */
+int testBitIs0( int k ) {
+
+   int i = k/32;
+   int pos = k%32;
+   unsigned int flag = 1;
+   flag = flag << pos;
+
+   int r = prime[i] & (1 << pos); 
+
+   if ( r == 0 )
+      return 1;        // return 1 to indicate that n is still prime
+   else
+      return 0;
+}
 
 
 void sieveOfE( int N )
@@ -16,21 +39,19 @@ void sieveOfE( int N )
 /*The array element with index i is true if the number i is prime and false otherwise. */
 /* 0 = false and 1=true*/
 
-	 int  isPrime[N+1];
 	 int i;
 	 int j;
 	 int k;
-	 int limit = N;
+	
+	for ( i = 0; i < N/MAX+1; i++ )
+      prime[i] = 0;
 
-	A[k/32] |= 1 << (N%32);  // Set the bit at the k-th position in A[i]
 
+	setBit( 0 );    // 0 is not prime
+   setBit( 1 );    // 1 is not prime
 
-	 isPrime[0] = 0; /* 0 is not prime */
-	 isPrime[1] = 0; /* 1 is not prime */
 	 
-	for (i = 2; i <= limit; i++ ){
-		 isPrime[i] = 1;   /* They are all candidates */     
-	 }
+
  k = 2;   // Start with 2 to find all primes
 
 
@@ -39,16 +60,16 @@ void sieveOfE( int N )
        ------------------------ */	  
 
 
-	      while ( k <= limit )
+	      while ( k <= N )
 	      {
 	
-		 for ( i = k; i <= limit; i++ )
-		     if ( isPrime[i] )
+		 for ( i = k; i <= N; i++ )
+		     if ( testBitIs0(i) )
 		        break;             // Found !
 
 
-		 for ( j = 2*i; j <= limit; j = j + i )
-		     isPrime[j] = 0;
+		 for ( j = 2*i; j <= N; j = j + i )
+		     setBit(j);
 	  
 		 k = i+1;
 	      }
@@ -58,7 +79,15 @@ void sieveOfE( int N )
 
 int countPrimes( int N ){
 /* this finds the number of primes that are less than or equal to N. */
+	int count, i;
 
+   count = 0;
+
+   for ( i = 2; i <= N; i++ )
+      if ( testBitIs0(i) )
+         count++;
+
+   return count;
 
 
 }
@@ -69,6 +98,11 @@ void printPrimes( int k1, int k2, int N ){
 /*This prints the prime numbers starting at the k1th prime 
 * and ending at the k2th prime for all primes ≤ N. 
 */
+	int i;
 
+   for ( i = 2; i <= N; i++ )
+      if ( testBitIs0(i) )
+         printf("%d ", i );
+   printf("\n\n");
 
 }
